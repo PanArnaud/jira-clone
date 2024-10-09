5eaf0703460f27e06e254c93e76ef0e9df4fd41f
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2024-10-10 00:16:51 +0400

    resolve lint errors

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -2,11 +2,6 @@
 
 import * as React from "react"
 import * as RechartsPrimitive from "recharts"
-import {
-  NameType,
-  Payload,
-  ValueType,
-} from "recharts/types/component/DefaultTooltipContent"
 
 import { cn } from "@/lib/utils"
 
@@ -74,7 +69,8 @@ ChartContainer.displayName = "Chart"
 
 const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
   const colorConfig = Object.entries(config).filter(
-    ([_, config]) => config.theme || config.color
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    ([_unused, config]) => config.theme || config.color
   )
 
   if (!colorConfig.length) {
@@ -361,10 +357,8 @@ function getPayloadConfigFromPayload(
 }
 
 export {
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-  ChartLegend,
+  ChartContainer, ChartLegend,
   ChartLegendContent,
-  ChartStyle,
+  ChartStyle, ChartTooltip,
+  ChartTooltipContent
 }
