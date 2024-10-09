9a222d3a447a678903eb575919039b62cb560fb3
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2024-10-09 22:31:18 +0400

    customize component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="destructive" size="sm">
-        Click me
+    <div className="">
+      <Input />
+      <Button variant="primary" size="xs">
+        Primary
       </Button>
-      <p className="text-red-500 font-semibold">Erika</p>
+      <Button variant="secondary" size="sm">
+        Secondary
+      </Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
     </div>
   );
 }
