3520dd4360a4d7f18ac55ae2c69ef6c0f2b89bcd
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2024-11-03 21:17:09 +0400

    Members list

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,7 +1,7 @@
 import "server-only";
 
 import { cookies } from "next/headers";
-import { Account, Client, Databases } from "node-appwrite";
+import { Account, Client, Databases, Users } from "node-appwrite";
 import { AUTH_COOKIE } from "@/features/auth/constants";
 
 export async function createSessionClient() {
@@ -36,6 +36,9 @@ export async function createAdminClient() {
   return {
     get account() {
       return new Account(client);
+    },
+    get users() {
+      return new Users(client);
     },
   };
 }
